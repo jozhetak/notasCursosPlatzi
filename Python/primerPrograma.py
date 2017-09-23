19f2b7fb46f251221772d3818d841f734a0c212b
osmandi <osmandi.github@gmail.com>
2017-09-23 19:29:34 -0400

    Usando TensorFlow con Python

---
+++ Python/primerPrograma.py
@@ -0,0 +1,37 @@
+# Hacer un cuadrado
+# Esto significa hacer una línea y dar vuelta 90° cuatro veces
+
+import turtle # Generar gráficos
+
+window = turtle.Screen()
+david = turtle.Turtle()
+
+# Generar una línea a la derecha
+david.forward(50)
+
+# Dar vuelta de 90°
+david.left(90)
+
+
+# Generar una línea a la derecha
+david.forward(50)
+
+# Dar vuelta de 90°
+david.left(90)
+
+
+# Generar una línea a la derecha
+david.forward(50)
+
+# Dar vuelta de 90°
+david.left(90)
+
+
+# Generar una línea a la derecha
+david.forward(50)
+
+# Dar vuelta de 90°
+david.left(90)
+
+# Para que turtle no nos cierre la ventana
+turtle.mainloop()
